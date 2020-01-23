e4bf1f9b1b829789a6ea1b23ad08834af3e82622
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2020-01-23 10:18:45 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Vedat Bilaloğlu on 23.01.2020.
+//  Copyright © 2020 Vedat Bilaloglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
